aeae317e59d344e1d6422579eb8263bd429a4e4d
Seepick <seepick@users.noreply.github.com>
2025-01-13 08:04:45 +0100

    cleanup

--- HolmesApp/version2/MyWeb/src/main/java/at/phudy/dao/ICatalogDao.java
+++
@@ -1,7 +0,0 @@
-package at.phudy.dao;
-
-import at.phudy.model.Catalog;
-
-public interface ICatalogDao extends IAbstractDao<Long, Catalog> {
-	// nothing to do
-}
--- StudWebsite/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#htlatex index "xhtml,2"
-
-#htlatex index.tex "myhtconfig" "" -dtarget
-
-htlatex index.tex "myhtconfig"
-
